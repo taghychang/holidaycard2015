c05c05773aaab943186091410103a6a9d4857ce5
Nick Colavita <ncolavit@TAG-NY-145.local>
2015-12-16 16:28:14 -0500

    proper placement of background

--- build/_src/demo_src.js
+++ build/_src/demo_src.js
@@ -20,6 +20,9 @@ demo.prototype = {
         game.load.image("img_10","images/demoPage/thermoCold.png");
     },
     create: function() {
+        demoPlaceholder = game.add.button(game.world.centerX, game.world.centerY, "demoPlaceholder");
+        demoPlaceholder.anchor.set(0.5, 0.5);
+
         var demoAsset_01 = game.add.sprite(225,350,'img_01');
         var demoAsset_02 = game.add.sprite(425,350,'img_02');
         var demoAsset_03 = game.add.sprite(420,540,'img_03');
@@ -27,6 +30,7 @@ demo.prototype = {
         var demoAsset_05 = game.add.sprite(185,690,'img_05');
         var demoAsset_06 = game.add.sprite(360,830,'img_06');
         var demoAsset_swipe = game.add.sprite(180,780,'img_swipe');
+
 
         // var demoAsset_07 = game.add.sprite(380,240,'img_07');
         // var demoAsset_08 = game.add.sprite(190,270,'img_08');
@@ -68,6 +72,10 @@ demo.prototype = {
         var tween6 = game.add.tween(demoAsset_06).to({alpha: 1}, 600);
         var tweenSwipe = game.add.tween(demoAsset_swipe).to({alpha: 1}, 600);
 
+        var tweenFadeOut = game.add.tween().to({alpha: 0}, 600);
+
+
+
         // var tween7 = game.add.tween(demoAsset_07).to({alpha: 1}, 1000, "Quart.easeOut", true, 2000);
         // var tween8 = game.add.tween(demoAsset_08).to({alpha: 1}, 500, "Quart.easeOut");
         // var tween9 = game.add.tween(demoAsset_09).to({alpha: 1}, 1000, "Quart.easeOut");
@@ -79,6 +87,7 @@ demo.prototype = {
         tween4.chain(tween5);
         tween5.chain(tween6);
         tween6.chain(tweenSwipe);
+        tweenSwipe.chain(tweenFadeOut);
 
         
         // tween6.chain(tween7);
@@ -95,11 +104,14 @@ demo.prototype = {
         // shopLoop.play();
         shopLoop.onDecoded.add(fadeTunesIn, this);
 
-        demoPlaceholder = game.add.button(game.world.centerX, game.world.centerY, "demoPlaceholder");
-        demoPlaceholder.variable = "PlayGame"; // next state
-        demoPlaceholder.inputEnabled = true;
-        demoPlaceholder.events.onInputDown.add(toNextState, this);
-        demoPlaceholder.anchor.set(0.5, 0.5);
+        // demoPlaceholder = game.add.button(game.world.centerX, game.world.centerY, "demoPlaceholder");
+        // demoPlaceholder.variable = "PlayGame"; // next state
+        // demoPlaceholder.inputEnabled = true;
+        // demoPlaceholder.events.onInputDown.add(toNextState, this);
+        // demoPlaceholder.anchor.set(0.5, 0.5);
+
+        // var sequence1 = game.add.group();
+        // sequence1.add(demoAsset_01)
 
         fadeIn();
     }
