787b4f6f9cf6cef5783cafffe766bb4f6bb0b9e1
Javi <jlopez@tagworldwide.com>
2015-12-16 22:56:04 -0500

    Implementing video for mobile

--- build/_src/pregame_src.js
+++ build/_src/pregame_src.js
@@ -1,26 +1,45 @@
-/* global console, Phaser: true, game*/
-
-
+/* global console, game, startDemo, toNextState, fadeIn */
 
 var preGame = function(game) {
     console.log(game);
 };
 
+// Added variable 
+var startButtonMobile, playBtn;
+
 preGame.prototype = {
 
-    preload: function() {
-
-
-    },
+    preload: function() { },
+    
     create: function() {
 
         // Create button //
         startButtonMobile = game.add.button(game.world.centerX, game.world.centerY, "startButtonMobile");
 
         if (game.device.desktop) {
-             startButtonMobile.variable = "Demo";
+             startButtonMobile.variable = "IntroVideo";
         } else {
-             startButtonMobile.variable = "Demo";
+            
+            // Add source to video tag & display 
+            var video_fallback = document.getElementById("video_fallback");
+            var video_source = document.createElement("source");
+            video_source.setAttribute("src", "video/intro.mp4");
+            video_fallback.appendChild(video_source);
+            video_fallback.style.display = "block";
+
+            // Hide canvas as it is created on the top most layer
+            var game_canvas = document.getElementsByTagName("canvas")[0];
+            game_canvas.style.display = "none";
+            
+            // When video ends force exit fullscreen, hide video player and bring back canvas
+            video_fallback.addEventListener("ended", function(){
+                video_fallback.webkitExitFullscreen();
+                video_fallback.style.display = "none";
+                game_canvas.style.display = "block";
+                
+                // Start Demo
+                startDemo(this);
+            });
         }
     
         // startButtonMobile.variable = "IntroVideo"; // next state
@@ -29,7 +48,7 @@ preGame.prototype = {
         startButtonMobile.anchor.set(0.5, 0.5);
 
              // Title
-        playBtn = game.add.image(223, 810, 'playBtn');
+        playBtn = game.add.image(223, 810, "playBtn");
 
 
         playBtn.alpha = 0;
